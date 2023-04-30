cabd1596465eca89969df7c7a4bd537183af34b4
Shivanker Goel <shivanker.goel@gmail.com>
2023-04-30 18:59:35 +0000

    Added single message GPT completion

--- agent_c.py
+++ agent_c.py
@@ -1,13 +1,20 @@
 #! /usr/bin/python3
 
 import logging as log
+import openai
+import os
 import requests
 
 log.basicConfig(
     level=log.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-
-OPENAI_API_KEY = "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"
+openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 def handle(msg, reply):
-    reply(f"I received: {msg}")
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": msg}
+        ]
+    )
+    reply(completion.choices[0].message["content"])
